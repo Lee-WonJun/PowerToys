ceb980f86a212ffb8d27ad93c7f37304ed63a8d4
WonJun Lee <dldnjs1013@nate.com>
2025-10-08 17:22:55 +0900

    LightSwitch: End of string literal

--- src/settings-ui/Settings.UI/SettingsXAML/Views/LightSwitchPage.xaml.cs
+++ src/settings-ui/Settings.UI/SettingsXAML/Views/LightSwitchPage.xaml.cs
@@ -127,7 +127,7 @@ namespace Microsoft.PowerToys.Settings.UI.Views
                 ViewModel.SelectedCity = null;
 
                 // CityAutoSuggestBox.Text = string.Empty;
-                ViewModel.SyncButtonInformation = $"{ViewModel.Latitude}°, {ViewModel.Longitude}°";
+                ViewModel.SyncButtonInformation = $"{ViewModel.Latitude}? {ViewModel.Longitude}?";
 
                 // ViewModel.CityTimesText = $"Sunrise: {result.SunriseHour}:{result.SunriseMinute:D2}\n" + $"Sunset: {result.SunsetHour}:{result.SunsetMinute:D2}";
                 SyncButton.IsEnabled = true;
@@ -152,7 +152,7 @@ namespace Microsoft.PowerToys.Settings.UI.Views
             }
             else if (ViewModel.ScheduleMode == "SunriseToSunsetGeo")
             {
-                ViewModel.SyncButtonInformation = $"{ViewModel.Latitude}°, {ViewModel.Longitude}°";
+                ViewModel.SyncButtonInformation = $"{ViewModel.Latitude}? {ViewModel.Longitude}?";
             }
 
             SunriseModeChartState();
